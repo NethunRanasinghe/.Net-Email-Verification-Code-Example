7bd5354964594f0a6b58b0bb13eea2d60c847051
Nethun Ranasingha <77114870+NethunRanasinghe@users.noreply.github.com>
2022-10-15 22:09:03 +0530

    Initial Commit

---
+++ Email_Code/Email.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Email_Code
+{
+    partial class Email
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Email));
+            this.btn_sendCode = new System.Windows.Forms.Button();
+            this.btn_checkcode = new System.Windows.Forms.Button();
+            this.txt_usr = new System.Windows.Forms.TextBox();
+            this.lbl_result = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_sendCode
+            // 
+            this.btn_sendCode.Location = new System.Drawing.Point(47, 12);
+            this.btn_sendCode.Name = "btn_sendCode";
+            this.btn_sendCode.Size = new System.Drawing.Size(378, 76);
+            this.btn_sendCode.TabIndex = 0;
+            this.btn_sendCode.Text = "Send Code";
+            this.btn_sendCode.UseVisualStyleBackColor = true;
+            this.btn_sendCode.Click += new System.EventHandler(this.btn_sendCode_Click);
+            // 
+            // btn_checkcode
+            // 
+            this.btn_checkcode.Location = new System.Drawing.Point(239, 125);
+            this.btn_checkcode.Name = "btn_checkcode";
+            this.btn_checkcode.Size = new System.Drawing.Size(186, 53);
+            this.btn_checkcode.TabIndex = 1;
+            this.btn_checkcode.Text = "Check Code";
+            this.btn_checkcode.UseVisualStyleBackColor = true;
+            this.btn_checkcode.Click += new System.EventHandler(this.btn_checkcode_Click);
+            // 
+            // txt_usr
+            // 
+            this.txt_usr.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt_usr.Location = new System.Drawing.Point(47, 125);
+            this.txt_usr.Multiline = true;
+            this.txt_usr.Name = "txt_usr";
+            this.txt_usr.Size = new System.Drawing.Size(174, 53);
+            this.txt_usr.TabIndex = 2;
+            // 
+            // lbl_result
+            // 
+            this.lbl_result.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.lbl_result.Font = new System.Drawing.Font("Bahnschrift Condensed", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lbl_result.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.lbl_result.Location = new System.Drawing.Point(0, 181);
+            this.lbl_result.Name = "lbl_result";
+            this.lbl_result.Size = new System.Drawing.Size(469, 99);
+            this.lbl_result.TabIndex = 3;
+            this.lbl_result.Text = "Result";
+            this.lbl_result.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Email
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.RosyBrown;
+            this.ClientSize = new System.Drawing.Size(469, 280);
+            this.Controls.Add(this.lbl_result);
+            this.Controls.Add(this.txt_usr);
+            this.Controls.Add(this.btn_checkcode);
+            this.Controls.Add(this.btn_sendCode);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Email";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Email";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_sendCode;
+        private System.Windows.Forms.Button btn_checkcode;
+        private System.Windows.Forms.TextBox txt_usr;
+        private System.Windows.Forms.Label lbl_result;
+    }
+}
---
+++ Email_Code/Email.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Email_Code
+{
+    public partial class Email : Form
+    {
+        public int num = 0;
+        public int generated_code = 0;
+
+        public Email()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_checkcode_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                num = int.Parse(txt_usr.Text);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Error : " + ex.Message);
+            }
+
+            if(num == generated_code && num > 100000)
+            {
+                lbl_result.Text = "Correct Number";
+            }
+            else
+            {
+                lbl_result.Text = "Wrong Number !";
+            }
+        }
+
+        private void btn_sendCode_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+
+            generated_code = rnd.Next(100000,1000000);
+
+            SmtpClient Client = new SmtpClient()
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential()
+                {
+                    UserName = "__Your_Email__",
+                    Password = "__Your_Password__"
+                }
+            };
+            MailAddress FromEmail = new MailAddress("From-email@gmail.com", "Subject);
+            MailAddress ToEmail = new MailAddress("To-email@gmail.com", "Someone");
+            MailMessage Message = new MailMessage()
+            {
+                From = FromEmail,
+                Subject = "New User Request Code",
+                Body = "New User has requested to Create an account.\nSend him/her this code him/her to proceed.\nCode : " + generated_code.ToString(),
+            };
+            Message.To.Add(ToEmail);
+
+            Client.SendCompleted += Client_SendCompleted;
+            Client.SendMailAsync(Message);
+        }
+
+        private void Client_SendCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            if(e.Error != null)
+            {
+                MessageBox.Show("Error : " + e.Error.Message);
+                return;
+            }
+            else
+            {
+                MessageBox.Show("Contact Admin to get the code.","Sent Successfully !");
+            }
+        }
+    }
+}
